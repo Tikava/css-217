4e159405398d73cea03577c0391b2debe9f77197
Tikava <wavoleniu@gmail.com>
2024-04-11 21:20:47 +0500

    Update character.py

--- assignment-5/ex-1/characters/character.py
+++ assignment-5/ex-1/characters/character.py
@@ -1,8 +1,7 @@
 class Character:
     def __init__(self, name, appearance, abilities, equipment, attributes):
-        self.name = name,
-        self.appearance = appearance,
-        self.abilities = abilities,
-        self.equipment = equipment,
+        self.name = name
+        self.appearance = appearance
+        self.abilities = abilities
+        self.equipment = equipment
         self.attributes = attributes
-        
