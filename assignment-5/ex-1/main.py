97e6c4bb736a4a8089b7bf208af914b2c386e761
Tikava <wavoleniu@gmail.com>
2024-04-11 21:19:57 +0500

    Update main.py

--- assignment-5/ex-1/main.py
+++ assignment-5/ex-1/main.py
@@ -1,48 +1,62 @@
 def battle(character1, character2):
     print(f"{character1.name} vs {character2.name}!\n")
 
-    while character1.attributes['health'] > 0 and character2.attributes['health'] > 0:
-        damage = character1.attributes['attack'] - character2.attributes['defense']
+    while character1.attributes[2].value > 0 and character2.attributes[2].value > 0:
+        damage = character1.attributes[0].value - character2.attributes[1].value
         if damage > 0:
-            character2.attributes['health'] -= damage
+            character2.attributes[2].value -= damage
             print(f"{character1.name} attacks {character2.name} for {damage} damage!")
         else:
             print(f"{character1.name}'s attack is ineffective against {character2.name}!")
 
-        if character2.attributes['health'] <= 0:
+            if damage <= 0 and character2.attributes[0].value - character1.attributes[1].value <= 0:
+                print("The battle has reached a stalemate! Neither character can harm the other.")
+                break
+
+        if character2.attributes[2].value <= 0:
             print(f"{character2.name} has been defeated!")
+            print("Character's customs: ", character2.__dict__)
             break
 
-        damage = character2.attributes['attack'] - character1.attributes['defense']
+        damage = character2.attributes[0].value - character1.attributes[1].value
         if damage > 0:
-            character1.attributes['health'] -= damage
+            character1.attributes[2].value -= damage
             print(f"{character2.name} attacks {character1.name} for {damage} damage!")
         else:
             print(f"{character2.name}'s attack is ineffective against {character1.name}!")
+            
+            if damage <= 0 and character1.attributes[0].value - character2.attributes[1].value <= 0:
+                print("The battle has reached a stalemate! Neither character can harm the other.")
+                break
 
-        if character1.attributes['health'] <= 0:
+        if character1.attributes[2].value <= 0:
             print(f"{character1.name} has been defeated!")
+            print("Character's customs: ", character1.__dict__)
             break
 
 def main():
     from character_creator import CharacterCreator
     from factory.character_factories.warrior_factory import WarriorFactory
-    from factory.character_factories.archer_factory import ArcherFactory
     from factory.character_factories.mage_factory import MageFactory
-    
+    from customization.customization_actions import customize_appearance, customize_abilities, customize_equipment, customize_attributes
+
     warrior_factory = WarriorFactory()
     mage_factory = MageFactory()
-    archer_factory = ArcherFactory()
 
     creator = CharacterCreator(warrior_factory)
 
     warrior = creator.create_character("Warrior")
     mage = CharacterCreator(mage_factory).create_character("Mage")
-
-    warrior.attributes = {"health": 100, "attack": 20, "defense": 10}
-    mage.attributes = {"health": 80, "attack": 25, "defense": 5}
+    
+    characters = [warrior, mage]
+    for character in characters:
+        customize_appearance(character)
+        customize_abilities(character)
+        customize_equipment(character)
+        customize_attributes(character)
+        print(f"Customization for {character.name} completed!")
 
     battle(warrior, mage)
-    
+
 if __name__ == "__main__":
-    main()    
+    main()
