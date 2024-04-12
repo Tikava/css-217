03fbd13db2b273cf4d1ebb75159728478139ad5d
Tikava <wavoleniu@gmail.com>
2024-04-12 14:38:59 +0500

    Update text_processor.py

--- assignment-5/ex-2/data_processing/processors/text_processor.py
+++ assignment-5/ex-2/data_processing/processors/text_processor.py
@@ -1,11 +1,22 @@
 from data_processing.processors.base_processor import DataProcessor
+from data_processing.data_storage import DataStorage
 
 class TextProcessor:
     def __init__(self, data):
         self.data = data
 
     def process(self):
-        print("Processing text:", self.data.content.text)
+        DataStorage().store_article(self.data.content["title"], 
+                ", ".join(self.data.content["authors"]), 
+                self.data.content["publicationYear"], 
+                self.data.content["abstract"], 
+                self.data.content["doi"], 
+                self.data.content["journal"], 
+                self.data.content["volume"], 
+                self.data.content["issue"], 
+                self.data.content["pages"], 
+                ", ".join(self.data.content["keywords"]))
+        print("Text saved:", self.data)
 
 class TextDataProcessor(DataProcessor):
     def create_data_processor(self, data):
