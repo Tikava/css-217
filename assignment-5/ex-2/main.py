f812bc8dea6e1d816b758c926c8d023fd50781e9
Tikava <wavoleniu@gmail.com>
2024-04-12 12:10:38 +0500

    Update main.py: video processing

--- assignment-5/ex-2/main.py
+++ assignment-5/ex-2/main.py
@@ -5,12 +5,15 @@ from data_processing.processors.text_processor import TextDataProcessor
 from data_processing.processors.video_processor import VideoDataProcessor
 from data_processing.data import Data
 from data_processing.fetchers.audio_fetcher import AudioFetcher
-from config.config import spotify_client_id, spotify_client_secret
+from data_processing.fetchers.video_fetcher import VideoFetcher
+from config.config import spotify_client_id, spotify_client_secret, youtube_data_api_key
 
 
 def main():
     
     audio_fethcer = AudioFetcher(spotify_client_id, spotify_client_secret)
+    video_fetcher = VideoFetcher(youtube_data_api_key)
+
     processor_creator = DataProcessorCreator()
 
 
@@ -27,24 +30,23 @@ def main():
         processor_creator.set_processor(TextDataProcessor())
         processor_creator.process_data(data)
     elif choice == "2":
-        query_field = input("Enter music name that you are looking for and to store in database: ")
-        audio_data = audio_fethcer.search_track(query_field)
-        if audio_data is not None:
-            data = Data("audio", audio_data)
+        music_name = input("Enter music name that you are looking for and to store in database: ")
+        music_info = audio_fethcer.search_track(music_name)
+        if music_info:
+            data = Data("audio", music_info)
             processor_creator.set_processor(AudioDataProcessor())
             processor_creator.process_data(data)
         else:
             print("Couldn't find specified audio.")
     elif choice == "3":
-        video_data = {
-            "name": "Video Name",
-            "artists": ["Artist 1", "Artist 2"],
-            "album": "Album Name",
-            "preview_url": "http://example.com/video.mp4"
-        }
-        data = Data("video", video_data)
-        processor_creator.set_processor(VideoDataProcessor())
-        processor_creator.process_data(data)
+        video_url = input("Enter youtube video link to get info and to store in database: ")
+        video_info = video_fetcher.get_video_info(video_url)
+        if video_info:
+            data = Data("video", video_info)
+            processor_creator.set_processor(VideoDataProcessor())
+            processor_creator.process_data(data)
+        else:
+            print("Failed to fetch video information.")
     else:
         print("Invalid choice. Please enter 1, 2, or 3.")
 
