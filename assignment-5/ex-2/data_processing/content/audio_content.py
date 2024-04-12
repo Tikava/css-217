ea339ad58343134d8b6dc27f8bab9ff889b6cae9
Tikava <wavoleniu@gmail.com>
2024-04-12 11:28:27 +0500

    Update audio_content.py

--- assignment-5/ex-2/data_processing/content/audio_content.py
+++ assignment-5/ex-2/data_processing/content/audio_content.py
@@ -1,3 +1,9 @@
 class AudioContent:
-    def __init__(self, audio_data):
-        self.audio_data = audio_data
+    def __init__(self, name, artists, album, preview_url):
+        self.name = name
+        self.artists = artists
+        self.album = album
+        self.preview_url = preview_url
+
+    def __str__(self):
+        return f"Audio Content: Name={self.name}, Artists={', '.join(self.artists)}, Album={self.album}, Preview URL={self.preview_url}"
