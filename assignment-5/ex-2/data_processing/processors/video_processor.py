6a757b110bc04678c3d4dcaa8e6974e260ad8b0c
Tikava <wavoleniu@gmail.com>
2024-04-12 12:15:56 +0500

    Update video_processor.py

--- assignment-5/ex-2/data_processing/processors/video_processor.py
+++ assignment-5/ex-2/data_processing/processors/video_processor.py
@@ -1,11 +1,18 @@
 from data_processing.processors.base_processor import DataProcessor
+from data_processing.data_storage import DataStorage
 
 class VideoProcessor:
     def __init__(self, data):
         self.data = data
 
     def process(self):
-        print("Processing video:", self.data.content.video_data)
+        DataStorage().store_video(
+            title=self.data.content['title'],
+            description=self.data.content['description'],
+            channel_title=self.data.content['channel_title'],
+            published_at=self.data.content['published_at']
+        )
+        print("Video saved:", self.data)
 
 class VideoDataProcessor(DataProcessor):
     def create_data_processor(self, data):
