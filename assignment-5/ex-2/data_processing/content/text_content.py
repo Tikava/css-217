228056d2cc660ed57ffde92c7a58ce8db089b7ff
Tikava <wavoleniu@gmail.com>
2024-04-12 14:38:29 +0500

    Update text_content.py

--- assignment-5/ex-2/data_processing/content/text_content.py
+++ assignment-5/ex-2/data_processing/content/text_content.py
@@ -1,3 +1,12 @@
 class TextContent:
-    def __init__(self, text):
-        self.text = text
+    def __init__(self, title, authors, publication_year, abstract, doi, journal, volume, issue, pages, keywords):
+        self.title = title
+        self.authors = authors
+        self.publication_year = publication_year
+        self.abstract = abstract
+        self.doi = doi
+        self.journal = journal
+        self.volume = volume
+        self.issue = issue
+        self.pages = pages
+        self.keywords = keywords
