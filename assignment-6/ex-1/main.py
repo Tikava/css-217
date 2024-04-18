c69825c4b878f59a7a783e082cbf6bc4a4dfa324
Tikava <wavoleniu@gmail.com>
2024-04-18 18:34:29 +0500

    Add assignment-6

---
+++ assignment-6/ex-1/main.py
@@ -0,0 +1,7 @@
+from database import Database
+
+def main():
+    db1 = Database('library_db')
+
+if __name__ == '__main__':
+    main()
---
+++ assignment-6/ex-1/models.py
@@ -0,0 +1,9 @@
+class Author:
+    def __init__(self, name):
+        self.name = name
+
+class Book:
+    def __init__(self, title, author, isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
